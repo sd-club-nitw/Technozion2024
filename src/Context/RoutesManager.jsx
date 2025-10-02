a1cab6d81860d31ed4dabc2bc3efea310bf5e154
sai rushith <mithintisairushith@gmail.com>
2025-10-02 19:14:34 +0530

    changes events route

--- src/Context/RoutesManager.jsx
+++ src/Context/RoutesManager.jsx
@@ -39,13 +39,13 @@ const RoutesManager = () => {
 			<Route path="*" element={<ComingSoon />} />
 			{/* <Route path="/about" element={<About />} /> */}
 			{/* <Route path="/sponsors" element={<Sponsors />} /> */}
-			{/* <Route path="/events" element={<Events />} /> */}
+			<Route path="/events" element={<Events />} />
 			{/* <Route path="/displayevents" element={<Displayevents />} /> */}
 			<Route path="/team" element={<Team />} />
 			{/* <Route path="/register" element={<Register />} /> */}
 			{/* <Route path="/gallery" element={<Gallery />} /> */}
-			{/* <Route path="/index" element={<Index />} /> */}
-			{/* <Route path="/card" element={<Card />} /> Route for the card component */}
+			<Route path="/index" element={<Index />} />
+			<Route path="/card" element={<Card />} /> Route for the card component
 
 			
 		</Routes>
