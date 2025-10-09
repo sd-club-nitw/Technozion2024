10adb12263c63f7c85231b380d20dc56652b12e3
rishi-2406 <ladrishi24@gmail.com>
2025-10-10 01:48:12 +0530

    fix

--- src/components/Register2/Register.jsx
+++ src/components/Register2/Register.jsx
@@ -184,44 +184,6 @@ const Register = () => {
 
 
   return (
-<<<<<<< HEAD
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 ">
-      {" "}
-      <div className="bg-gray p-8 rounded-2xl shadow-lg w-full max-w-md">
-        {" "}
-        <h2 className="text-2xl font-bold mb-6 text-center">
-          Create Account
-        </h2>{" "}
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          {" "}
-          <input
-            type="text"
-            placeholder="Full Name"
-            {...reactRegister("name", { required: "Name is required" })}
-            className="w-full px-4 py-2 border rounded-md outline-none focus:outline-none bg-gray"
-          />{" "}
-          {errors.name && (
-            <p className="text-ssred text-sm">{errors.name.message}</p>
-          )}{" "}
-          <input
-            type="email"
-            placeholder="Email"
-            {...reactRegister("email", { required: "Email is required" })}
-            className="w-full px-4 py-2 border rounded-md outline-none focus:outline-none bg-gray"
-          />{" "}
-          {errors.email && (
-            <p className="text-red-500 text-sm">{errors.email.message}</p>
-          )}{" "}
-          <input
-            type="password"
-            placeholder="Password"
-            {...reactRegister("password", { required: "Password is required" })}
-            className="w-full px-4 py-2 border rounded-md outline-none focus:outline-none bg-gray"
-          />{" "}
-          {errors.password && (
-            <p className="text-red-500 text-sm">{errors.password.message}</p>
-          )}{" "}
-=======
     <div className="flex justify-center items-center md:items-start min-h-screen p-2 sm:p-4 md:pt-24 overflow-auto bg-gray text-gray-300">
 
       {isEventsModalOpen && <EventsModal />}
@@ -465,7 +427,6 @@ const Register = () => {
             <div className="text-red-400 text-sm mt-2">{paymentError}</div>
           )}
 
->>>>>>> c569a10bf3cf4169c980c6b9f3a4f58845d462b7
           <button
             type="submit"
             className="w-full py-3 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition font-semibold shadow-md"
