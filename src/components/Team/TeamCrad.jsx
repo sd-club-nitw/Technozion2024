2210071abe13cc0e74f3fc12617bd4503d357033
[abhiraj] <abhiraj.dustakar@gmail.com>
2025-10-20 00:20:55 +0530

    updated team-card

--- src/components/Team/TeamCrad.jsx
+++ src/components/Team/TeamCrad.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./TeamCrad.css";
 
-function TeamCrad({src, name, position, email=""}) {
+const TeamCard = ({ src, name, position, email = "" }) => {
   return (
-    <div className="flex flex-col bg-slate-800 rounded-lg p-3 max-w-80">
-      <div className="flex-1 flex items-center justify-center">
-        <img
-          src={src}
-          alt={name}
-          className="w-full object-cover aspect-[11/12] rounded-lg"
-        />
-      </div>
-      <div className="mt-3 px-1">
-        <p className="font-bold text-white uppercase text-md">{name}</p>
-        <p className="text-teal-400 text-base opacity-60">{position}</p>
-    {email && <div  className="text-teal-400 opacity-40 text-base cursor-pointer" >{email}</div>}
-      </div>
-    </div>
-  )
-}
+    <>
+      <div className="teamcard-wrapper">
+        <div className="teamcard-title">{position}</div>
 
-export default TeamCrad
+        <div className="teamcard">
+          <img src={src} alt={name} />
+        </div>
+
+        <div className="teamcard-info">
+          <p className="teamcard-name">{name}</p>
+          {email && <p className="teamcard-email">{email}</p>}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TeamCard;
